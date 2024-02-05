edfc71328381bdb4e006819b55972df83e6daf38
Ladan <lada0080@stud.kea.dk>
2024-02-05 15:41:55 +0100

    single virker

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 window.addEventListener("DOMContentLoaded", init);
+
+const modeURL = "https://kea-alt-del.dk/t7/api/products";
+
+let modeTemplate;
+let modeContainer;
+
+function init() {
+  console.log("init");
+
+  modeTemplate = document.querySelector(".mode_template");
+  console.log("mode_template", modeTemplate);
+
+  modeContainer = document.querySelector(".mode_container");
+  console.log("mode_container", modeContainer);
+
+  fetch(modeURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      showProduct(json);
+    });
+}
+
+function showProduct(beerJSON) {
+  let productClone;
+
+  beerJSON.forEach((product) => {
+    console.log("product", product);
+    productClone = modeTemplate.cloneNode(true).content;
+    productClone.querySelector(".product_image").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    productClone.querySelector(".product_image").alt = `Picture of ${product.name} clothes`;
+    productClone.querySelector(".produkt_titel").textContent = product.productdisplayname;
+    productClone.querySelector(".mode_pris").textContent = product.price;
+    modeContainer.appendChild(productClone);
+  });
+}
---
+++ single.js
@@ -0,0 +1,11 @@
+const parametre = new URLSearchParams(window.location.search);
+const id = parametre.get("id");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
+  .then((res) => res.json())
+  .then(vis);
+
+function vis(data) {
+  console.log(data);
+  document.querySelector("h2").textContent = data.productdisplayname;
+}
