81cddc35fe5ad3bc8803954fd30d556dcb03a2be
Ladan <lada0080@stud.kea.dk>
2024-02-10 18:15:14 +0100

    færdig

--- single.js
+++ single.js
@@ -9,7 +9,10 @@ function vis(data) {
   console.log(data);
   document.querySelector("h2").textContent = data.productdisplayname;
   document.querySelector("p").textContent = data.price;
+  document.querySelector("varetxt").textContent = data.articletype;
   document.querySelector("h3").textContent = data.productdisplayname;
   document.querySelector(".model").textContent = data.productdisplayname;
   document.querySelector(".id").textContent = data.id;
 }
+
+// virker ikkje for andre varer eller nike t shirt tekst på andre varer. men kun for den første t shirt
